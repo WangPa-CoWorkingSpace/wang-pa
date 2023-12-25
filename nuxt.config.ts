112e838ef6d3eaccc3c24260eb94cd53ebb3a04f
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-12-25 15:54:09 +0700

    init nuxt vue

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default ({
+  devtools: { enabled: true },
+  experimental: {
+    payloadExtraction: false,
+  },
+  components: {
+    dirs: ["~/components"],
+  },
+  css: ['~/assets/styles.css'],
+  modules: [
+    "@nuxtjs/tailwindcss",
+  ],
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      link: [
+        { rel: 'stylesheet', href: 'https://pro.fontawesome.com/releases/v5.15.0/css/all.css' },
+      ],
+      title: 'Wang-Pa',
+      script: [],
+    }
+  }
+})
