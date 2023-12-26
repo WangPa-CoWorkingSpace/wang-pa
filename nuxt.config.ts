3eddd6c757dca606642ecb66f997bc3360dc130d
Kankawee Aramrak <kankaweeisci123@gmail.com>
2023-12-26 23:38:43 +0700

    (UI,Bug): navbar-bg not change when scroll

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  ssr: true,
+  devtools: { enabled: false },
+  ssr: false,
   experimental: {
     payloadExtraction: false,
   },
