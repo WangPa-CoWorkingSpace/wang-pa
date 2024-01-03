695f2b29898841a2605c6669a9a90ec1053aebd6
Kankawee Aramrak <kankaweeisci123@gmail.com>
2024-01-03 22:24:09 +0700

    (UI): Improving Navbar of Desktop

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="navbar" class="fixed z-40 w-full transition-all duration-[1200ms]"
     :class="{ 'bg-[#1cb6d9e5]': pageScroll, 'bg-black/20': istoggle && !pageScroll }">
-    <div class="flex justify-between min-h-[60px] items-center px-2 border-1">
-      <div>
+    <div class="flex justify-between min-h-[60px] items-center px-2 border-1 lg:justify-end">
+      <div class="lg:hidden">
         <button @click.prevent="hamberBTN" class="text-[30px] mt-1 p-2"
           :class="{ 'text-white': pageScroll, 'fas fa-user-circle': !user_avatar_CK }">
           <NuxtImg class="rounded-full w-[30px] border-[1px] border-white" :src="user_avatar_CK || '/'"
             :class="{ 'hidden': !user_avatar_CK }"></NuxtImg>
         </button>
       </div>
-      <nav class="items-center gap-[30px] font-normal hidden lg:flex"
+      <nav class="items-center gap-[30px] font-normal hidden lg:flex lg:gap-[20px]"
         :class="{ 'text-white': pageScroll, 'text-black': !pageScroll }">
         <div>
           <NuxtLink to="/">Home</NuxtLink>
@@ -18,12 +18,26 @@
           <NuxtLink :class="{ 'hidden': user_email_CK || user_avatar_CK || user_full_name_CK }" to="/login">Login
           </NuxtLink>
         </div>
-        <div>
-          <NuxtLink @click.prevent="logoutBTN"
-            :class="{ 'hidden': !user_email_CK || !user_avatar_CK || !user_full_name_CK }">Logout</NuxtLink>
+        <!-- Middle lines -->
+        <div class="min-h-[30px] w-[3px] bg-black"></div>
+        <!-- In Middle Line Items -->
+        <NuxtLink class="fab fa-discord text-[30px] text-black" to="https://github.com/WangPa-CoWorkingSpace"
+          target="_blank"></NuxtLink>
+        <NuxtLink class="fab fa-github text-[30px] text-black" to="" target="_blank"></NuxtLink>
+        <div class="min-h-[30px] w-[3px] bg-black"></div>
+        <!-- User Profile -->
+        <div :class="{ 'hidden': !user_email_CK || !user_avatar_CK || !user_full_name_CK }">
+          <NuxtLink @click.prevent="logoutBTN">Logout</NuxtLink>
+        </div>
+        <div class="hidden lg:block pr-[30px]">
+          <button @click.prevent="hamberBTN" class="text-[30px]"
+            :class="{ 'text-white': pageScroll, 'fas fa-user-circle': !user_avatar_CK }">
+            <NuxtImg class="rounded-full w-[30px] border-[1px] border-white" :src="user_avatar_CK || '/'"
+              :class="{ 'hidden': !user_avatar_CK }"></NuxtImg>
+          </button>
         </div>
       </nav>
-      <button class="lg:hidden grid gap-1 mt-1 bg-transparent p-2 select-none" @click.prevent="hamberBTN">
+      <button class="lg:hidden grid gap-1 mt-0 bg-transparent p-2 select-none" @click.prevent="hamberBTN">
         <span id="ham1" :class="{ 'rotate-45 translate-y-[9px]': istoggle == true, 'bg-white': pageScroll }"
           class="w-6 h-1 bg-black rounded-[10px] transition-all duration-200"></span>
         <span id="ham2" :class="{ 'opacity-0': istoggle == true, 'bg-white': pageScroll }"
@@ -32,8 +46,8 @@
           class="w-6 h-1 bg-black rounded-[10px] transition-all duration-200"></span>
       </button>
     </div>
-    <div id="mobileMenu"
-      class="lg:hidden justify-center space-y-2 text-center transition-all duration-200 overflow-hidden"
+    <!-- Mobile Menu -->
+    <div class="lg:hidden justify-center space-y-2 text-center transition-all duration-200 overflow-hidden"
       :class="{ 'text-white': pageScroll, 'text-black': !pageScroll, 'h-0': !istoggle, 'h-[100px]': istoggle }">
       <div>
         <NuxtLink @click.prevent="hamberBTN" to="/">
@@ -44,10 +58,8 @@
           to="/login">
           Login</NuxtLink>
       </div>
-      <div>
-        <NuxtLink @click.prevent="logoutBTN"
-          :class="{ 'hidden': !user_email_CK || !user_avatar_CK || !user_full_name_CK }">
-          Logout</NuxtLink>
+      <div :class="{ 'hidden': !user_email_CK || !user_avatar_CK || !user_full_name_CK }">
+        <NuxtLink @click.prevent="logoutBTN">Logout</NuxtLink>
       </div>
     </div>
   </div>
