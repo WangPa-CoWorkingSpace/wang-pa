814ccd6a8dd3624d3f13118cbac0b2ccf31d790c
Kankawee Aramrak <kankaweeisci123@gmail.com>
2024-01-14 16:48:51 +0700

    (UI): Improve findPage for mobile from niji v.

--- pages/find.vue
+++ pages/find.vue
@@ -9,97 +9,62 @@
             <i class="fas fa-location-arrow text-white text-[15px] p-2"></i>
         </button>
     </div>
-    <div class="xl:grid container ml-8 
-            xl:grid-cols-5 xl:mx-16 xl:mt-4 xl:pb-16
-            ">
-        <div class="mx-4 xl:col-span-2">
-            <div class="flex justify-center mb-4"><h1 class="text-[24px]">แสดงความคิดเห็นและให้คะแนน</h1></div>
-            <div class="h-16">
-                <div class="inline-flex">
-                    <h4 class="pr-3">ระดับคะแนน</h4>
-                    <div class="text-yellow-400 cursor-pointer">
-                        <i
-                            v-for="(star, index) in stars"
-                            :key="index"
-                            :class="{ 'fas fa-star': star === 'full', 'far fa-star': star === 'empty' }"
-                            @click="rate(index + 1)"
-                        ></i>
-                    </div>
+    <div class="xl:grid container xl:grid-cols-5 xl:mx-16 xl:mt-4 xl:pb-16">
+        <div class="px-4 xl:col-span-2">
+            <div class="flex justify-center mt-[30px]">
+                <h1 class="text-[24px]">รีวิว</h1>
+            </div>
+            <div class="space-y-[20px]">
+                <div class="text-yellow-400 text-[20px] cursor-pointer">
+                    <i v-for="(star, index) in stars" :key="index"
+                        :class="{ 'fas fa-star': star === 'full', 'far fa-star': star === 'empty' }"
+                        @click="rate(index + 1)"></i>
                 </div>
-                <div class="my-2"><h4>ความคิดเห็นของคุณ</h4></div>
-                <div class="mt-1"><input
-                    class="border-[2px] border-black/50 rounded-[20px] px-2 pb-[100px] py-1 min-h-[100px] w-[460px] placeholder:text-black/50 placeholder:text-[16px] focus:outline-none
+                <div><input class="border-[2px] border-black/50 rounded-[20px] px-2 pb-[100px] py-1 min-h-[100px] w-[300px] placeholder:text-black/50 placeholder:text-[16px] focus:outline-none
                         xl:w-full
                         " placeholder="เขียนรีวิวของคุณ">
                 </div>
             </div>
             <!-- Default -->
-            <div class="bg-blue-400 rounded-[25px] w-48 py-1.5 px-[5px] mt-[148px] flex justify-center items-center relative
+            <div class="bg-[#1cb7d9] rounded-[25px] w-max py-[5px] px-[10px] mt-[50px] flex justify-center items-center relative
             xl:mt-36
                     "><button class="text-white"><i class="far fa-paper-plane mr-2"></i>ส่งความคิดเห็น</button>
             </div>
         </div>
-        
-        <div class="col-span-1 pt-16 mx-4
-                xl:col-span-3 xl:pt-0">
-            <div class="flex justify-center"><h1 class="text-[24px]">สถานที่ใกล้เคียง</h1></div>
-            <!-- Mobile -->
-            <Carousel class="xl:hidden select-none" :itemsToShow="1" :wrapAround="true" :transition="500">
-                <Slide v-for="(slide, index) in slides_data_near_me" :key="index">
-                    <div class="carousel__item py-8 w-[270px]">
-                        <div class="bg-white rounded-[10px] shadow-[0_0_20px_0_rgba(0,0,0,0.25)]">
-                            <NuxtImg class="rounded-t-[10px] w-[300px] h-[256px] object-cover" :src="slide.image" :alt="slide.title"
-                            objectFit='contain' loading="lazy" />
-                            <div class="p-4">
-                                <h1 class="text-black font-medium text-[20px] text-left w-full h-[30px] overflow-hidden">{{ slide.title }} </h1>
-                                <h4 class="text-[18px] text-left text-[#1cb7d9]">{{ slide.price }}</h4>
-                                <div class="h-[10px] w-full flex text-black/50 space-x-2 mb-5">
-                                    <i v-for="feature in slide.features" :class="`fas fa-${feature}`" :key="feature"></i>
+
+        <div class="flex justify-center mt-[50px]">
+            <h1 class="text-[24px]">สถานที่ใกล้เคียง</h1>
+        </div>
+        <!-- Mobile -->
+
+        <Carousel class="xl:hidden select-none" :itemsToShow="1" :wrapAround="true" :transition="500">
+            <Slide v-for="(slide, index) in slides_data_near_me" :key="index">
+                <div class="carousel__item py-8 w-[270px]">
+                    <div class="bg-white rounded-[10px] shadow-[0_0_20px_0_rgba(0,0,0,0.25)]">
+                        <NuxtImg class="rounded-t-[10px] w-[300px] h-[256px] object-cover" :src="slide.image"
+                            :alt="slide.title" objectFit='contain' loading="lazy" />
+                        <div class="p-4">
+                            <h1 class="text-black font-medium text-[20px] text-left w-full h-[30px] overflow-hidden">{{
+                                slide.title }} </h1>
+                            <h4 class="text-[18px] text-left text-[#1cb7d9]">{{ slide.price }}</h4>
+                            <div class="h-[10px] w-full flex text-black/50 space-x-2 mb-5">
+                                <i v-for="feature in slide.features" :class="`fas fa-${feature}`" :key="feature"></i>
+                            </div>
+                            <h4 class="text-[15px] text-left text-black/50">{{ slide.openingHours }}</h4>
+                            <div class="flex justify-between items-center mt-[30px]">
+                                <div class="text-yellow-400">
+                                    <i v-for="star in slide.stars" :key="star" :class="`${star} fa-star`"></i>
                                 </div>
-                                <h4 class="text-[15px] text-left text-black/50">{{ slide.openingHours }}</h4>
-                                <div class="flex justify-between items-center mt-[30px]">
-                                    <div class="text-yellow-400">
-                                        <i v-for="star in slide.stars" :key="star" :class="`${star} fa-star`"></i>
-                                    </div>
-                                    <div class="h-max w-max bg-[#20DE33] text-white text-[12px] py-[3px] px-[10px] rounded-[20px]">
-                                        <h4>{{ slide.capacity }}</h4>
-                                    </div>
+                                <div
+                                    class="h-max w-max bg-[#20DE33] text-white text-[12px] py-[3px] px-[10px] rounded-[20px]">
+                                    <h4>{{ slide.capacity }}</h4>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </Slide>
-            </Carousel>
-            <!-- Desktop -->
-            <Carousel class="hidden xl:block select-none" ref="near_me_slideLG" :itemsToShow="3" :wrapAround="true"
-                :transition="500">
-                <Slide v-for="(slide, index) in slides_data_near_me" :key="index">
-                <div class="carousel__item py-8 w-[200px]">
-                    <div class="bg-white rounded-[10px] shadow-[0_0_20px_0_rgba(0,0,0,0.25)]">
-                    <NuxtImg class="rounded-t-[10px] w-[200px] h-[175px] object-cover" :src="slide.image" :alt="slide.title"
-                        objectFit='contain' loading="lazy" />
-                    <div class="p-4">
-                        <h1 class="text-black font-medium text-[18px] text-left w-full h-[30px] overflow-hidden">{{ slide.title }}
-                        </h1>
-                        <h4 class="text-[16px] text-left text-[#1cb7d9]">{{ slide.price }}</h4>
-                        <div class="h-[10px] w-full flex text-black/50 space-x-2 mb-5">
-                        <i v-for="feature in slide.features" :class="`fas fa-${feature}`" :key="feature"></i>
-                        </div>
-                        <h4 class="text-[13px] text-left text-black/50">{{ slide.openingHours }}</h4>
-                        <div class="flex justify-between items-center mt-[30px]">
-                        <div class="text-yellow-400 text-[14px]">
-                            <i v-for="star in slide.stars" :key="star" :class="`${star} fa-star`"></i>
-                        </div>
-                        <div class="h-max w-max bg-[#20DE33] text-white text-[10px] py-[3px] px-[10px] rounded-[20px]">
-                            <h4>{{ slide.capacity }}</h4>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
                 </div>
-                </Slide>
-            </Carousel>
-        </div>
+            </Slide>
+        </Carousel>
     </div>
     <Footer />
 </template>
@@ -139,7 +104,7 @@ export default defineComponent({
         Slide,
     },
     async setup() {
-              
+
         const mapContainer = ref(null);
         const map = ref<Map | null>(null);
         const markers = ref<Marker[]>([]);
@@ -207,8 +172,8 @@ export default defineComponent({
                                 <p class="text-black text-[14px] font-medium flex justify-center bg-white rounded-[10px] mt-[5px]">${price}</p>
                             </div>`;
 
-                                            const cws_image_element = dataItem.cws_image.map(imageSrc =>
-                                                `<div class="col-image">
+                        const cws_image_element = dataItem.cws_image.map(imageSrc =>
+                            `<div class="col-image">
                                 <img class="rounded-[5px] w-[95px] h-[60px] object-cover" src="${imageSrc}">
                             </div>`
                         ).join('');
@@ -320,35 +285,20 @@ export default defineComponent({
             }
         };
 
-        //Form Update
-        function formReviewUpdate(input_name: string) {
-        switch (input_name) {
-            case 'name':
-            Cookies.set('cws_name_form_Cookie', encodeURIComponent(cws_name_form.value));
-            break
-            case 'price':
-            Cookies.set('cws_price_form_Cookie', encodeURIComponent(cws_price_form.value));
-            break
-            case 'review':
-            Cookies.set('cws_name_review_Cookie', encodeURIComponent(cws_review_form.value));
-            break
-        }
-        }
-
         const stars = ref(['empty', 'empty', 'empty', 'empty', 'empty']);
         const selectedRating = ref(0);
 
-        const rate = (rating) => {
+        const rate = (rating: number) => {
             selectedRating.value = rating;
             stars.value = stars.value.map((_, index) =>
                 index < rating ? 'full' : 'empty'
             );
         };
 
-        return { 
-            mapContainer, 
-            goToCurrentLocation, 
-            slides_data_near_me, 
+        return {
+            mapContainer,
+            goToCurrentLocation,
+            slides_data_near_me,
             stars,
             selectedRating,
             rate,
