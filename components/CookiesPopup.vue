39fc3aa45f3b3f7b7440a330ef439505f7bfb8d2
DucKyWay <niji2549@gmail.com>
2024-01-24 22:20:53 +0700

    (UI): typo in cookies popup

--- components/CookiesPopup.vue
+++ components/CookiesPopup.vue
@@ -2,7 +2,7 @@
     <div class="fixed w-screen h-[100px] bottom-0 rounded-t-[20px] z-40 bg-gradient-to-r from-[#59C8F7] to-white to-[120%] xl:to-[140%] xl:w-[40%] xl:right-0" :class="{'hidden': alreadyCookies === 'true'}">
         <div class="flex justify-between items-center h-full px-[20px]">
             <h4 class="text-[14px] w-[70%] text-white font-extralight">เราใช้คุกกี้เพื่อปรับปรุงประสบการณ์ของคุณในเว็บไซต์ของเรา
-                ด้วยการใช้บริการของเรานี้คุณยินยอมให้เราใช้คุกกี้ตาม <NuxtLink to="/cookies" target="_blank" class="underline font-medium">นโยบายคุ้กกี้</NuxtLink> ของเรา
+                ด้วยการใช้บริการของเรานี้คุณยินยอมให้เราใช้คุกกี้ตาม <NuxtLink to="/cookies" target="_blank" class="underline font-medium">นโยบายคุกกี้</NuxtLink> ของเรา
             </h4>
             
             <button @click.prevent="AcceptCookies" class="bg-white rounded-[10px] px-[20px]">
