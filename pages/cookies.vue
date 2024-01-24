9bfe8221c7cad68f01176e581af9150f635c6501
DucKyWay <niji2549@gmail.com>
2024-01-24 19:44:34 +0700

    (UI): create cookies.vue for policy

---
+++ pages/cookies.vue
@@ -0,0 +1,27 @@
+<template>
+    <Navbar />
+        <div class="container mx-auto px-9 py-9 xl:px-4 xl:py-8">
+            <h1 class="text-[48px] flex justify-center mt-5">นโยบายคุกกี้</h1>
+            <div class="mt-5">
+                <h4 class="text-[28px]">คุกกี้คืออะไร</h4>
+                <h6>คุกกี้คือไฟล์ข้อความขนาดเล็กที่เก็บไว้ในคอมพิวเตอร์ของคุณโดยเว็บเบราว์เซอร์ตามคำขอของไซต์ที่คุณกำลังดู สิ่งนี้ช่วยให้เว็บไซต์ที่คุณกำลังดูจดจำสิ่งต่าง ๆ เกี่ยวกับตัวคุณเช่นค่ากำหนดและประวัติหรือเพื่อให้คุณเข้าสู่ระบบ คุกกี้อาจถูกเก็บไว้ในคอมพิวเตอร์ของคุณในช่วงเวลาสั้น ๆ (เช่นในขณะที่เบราว์เซอร์ของคุณเปิด) หรือเป็นเวลานานแม้เป็นปี คุกกี้ที่ไม่ได้ตั้งค่าโดยไซต์นี้จะไม่สามารถเข้าถึงเราได้</h6>
+            </div>
+            <div class="mt-5">
+                <h4 class="text-[28px]">การลบ / ปิดการใช้งานคุกกี้</h4>
+                <h6>การจัดการการตั้งค่าคุกกี้และคุกกี้ของคุณจะต้องทำจากภายในตัวเลือก / การตั้งค่าเบราว์เซอร์ของคุณ นี่คือรายการคำแนะนำเกี่ยวกับวิธีการทำเช่นนี้สำหรับซอฟต์แวร์เบราว์เซอร์ยอดนิยม</h6>
+                <p>1. <a class="text-blue-400" href="https://support.microsoft.com/en-gb/windows/delete-and-manage-cookies-168dab11-0753-043d-7c16-ede5947fc64d">Microsoft Internet Explorer</a></p>
+                <p>2. <a class="text-blue-400" href="https://support.microsoft.com/en-us/windows/microsoft-edge-browsing-data-and-privacy-bb8174ba-9d73-dcf2-9b4a-c582b4e640dd">Microsoft Edge</a></p>
+                <p>3. <a class="text-blue-400" href="https://support.mozilla.org/en-US/kb/cookies-information-websites-store-on-your-computer">Mozilla Firefox</a></p>
+                <p>4. <a class="text-blue-400" href="https://support.google.com/chrome/answer/95647">Google Chrome</a></p>   
+                <p>5. <a class="text-blue-400" href="https://support.apple.com/en-gb/guide/safari/sfri11471/mac">Safari สำหรับ macOS</a></p>
+                <p>6. <a class="text-blue-400" href="https://support.apple.com/en-gb/105082">Safari สำหรับ iOS</a></p>
+            </div>
+            <div class="mt-5">
+                <h4 class="text-[28px]">ข้อมูลเพิ่มเติมเกี่ยวกับคุกกี้</h4>
+                <h6>หากต้องการเรียนรู้เพิ่มเติมเกี่ยวกับคุกกี้และค้นหาข้อมูลเพิ่มเติมเกี่ยวกับการปิดกั้นคุกกี้บางประเภทโปรดไปที่เกี่ยวกับคุกกี้ กรุณาเยี่ยมชม 
+                    <a class="text-blue-400" href="https://ico.org.uk/for-the-public/online/cookies/">ICO website Cookies</a>
+                </h6>
+            </div>
+        </div>
+        <div class="mt-1"><Footer /></div>
+</template>
